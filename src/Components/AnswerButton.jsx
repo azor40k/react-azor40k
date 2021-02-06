a58c9c51a71950c1d1e7acbcab74fc5c299014d5
ProLyfe <rayane.prolyfe@gmail.com>
2021-02-06 16:58:09 +0100

    [add] questions number increment when next question

--- src/Components/AnswerButton.jsx
+++ src/Components/AnswerButton.jsx
@@ -1,14 +1,15 @@
 import '../Sass/components/AnswerButton.scss';
 import { useState } from 'react';
 
-const AnswerButton = ({ color, answer, setQuestionNb, changeToGray, setBaseColors }) => {
+const AnswerButton = ({ color, answer, setQuestionNb, changeToGray, setBaseColors, setScore, setQuestionIncrement }) => {
 
   const submitAnswer = () => {
         const targetAnswer = answer[1]
         changeToGray();
         setTimeout(() => {
             setQuestionNb(prev => prev + 1 );
-            setBaseColors(["red", "blue", "orange", "green"]);  
+            setBaseColors(["red", "blue", "orange", "green"]);
+            setQuestionIncrement(prevState => prevState + 1);  
         }, 2000)         
   }
   return (
--- src/Components/Answers.jsx
+++ src/Components/Answers.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import '../Sass/components/Answers.scss';
 import AnswerButton from './AnswerButton'
 
-const Answers = ({ setQuestionNb, answers }) => {
+const Answers = ({ setQuestionNb, answers, setQuestionIncrement, setScore }) => {
     
     const [baseColors, setBaseColors] = useState(["red", "blue", "orange", "green"]);
     const { answer1, answer2, answer3, answer4 } = answers;
@@ -15,10 +15,10 @@ const Answers = ({ setQuestionNb, answers }) => {
 
     return (
         <section className="Answers">
-            <AnswerButton color={baseColors[0]} answer={answer1} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors}/>
-            <AnswerButton color={baseColors[1]} answer={answer2} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors}/>
-            <AnswerButton color={baseColors[2]} answer={answer3} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors}/>
-            <AnswerButton color={baseColors[3]} answer={answer4} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors}/>
+            <AnswerButton color={baseColors[0]} answer={answer1} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement}/>
+            <AnswerButton color={baseColors[1]} answer={answer2} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement}/>
+            <AnswerButton color={baseColors[2]} answer={answer3} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement}/>
+            <AnswerButton color={baseColors[3]} answer={answer4} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement}/>
         </section>
 )};
 
