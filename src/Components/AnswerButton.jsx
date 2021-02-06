c60458066c609704614e9914a0865b445cde207b
ProLyfe <rayane.prolyfe@gmail.com>
2021-02-06 17:07:37 +0100

    [add] increment score

--- src/Components/AnswerButton.jsx
+++ src/Components/AnswerButton.jsx
@@ -4,13 +4,15 @@ import { useState } from 'react';
 const AnswerButton = ({ color, answer, setQuestionNb, changeToGray, setBaseColors, setScore, setQuestionIncrement }) => {
 
   const submitAnswer = () => {
-        const targetAnswer = answer[1]
+      //   const targetAnswer = answer[1]
+        answer[1] === true ? setScore(prevScore => prevScore + 1) : console.log('FAUX');
+        
         changeToGray();
         setTimeout(() => {
             setQuestionNb(prev => prev + 1 );
             setBaseColors(["red", "blue", "orange", "green"]);
             setQuestionIncrement(prevState => prevState + 1);  
-        }, 2000)         
+        }, 1000)         
   }
   return (
         <button className={ `AnswerButton ${color}`} onClick={submitAnswer}>{answer[0]}</button>
