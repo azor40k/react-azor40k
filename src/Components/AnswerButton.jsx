6ef02686a0da87025538026a2fe4c0adc7380478
ProLyfe <rayane.prolyfe@gmail.com>
2021-02-06 01:18:14 +0100

    [fix] clean up the code

--- src/Components/AnswerButton.jsx
+++ src/Components/AnswerButton.jsx
@@ -3,16 +3,13 @@ import { useState } from 'react';
 
 const AnswerButton = ({ color, answer, setQuestionNb, changeToGray, setBaseColors }) => {
 
-
-
   const submitAnswer = () => {
         const targetAnswer = answer[1]
         changeToGray();
         setTimeout(() => {
             setQuestionNb(prev => prev + 1 );
             setBaseColors(["red", "blue", "orange", "green"]);  
-        }, 2000) 
-        // setBaseColors(["red", "blue", "orange", "green"]);         
+        }, 2000)         
   }
   return (
         <button className={ `AnswerButton ${color}`} onClick={submitAnswer}>{answer[0]}</button>
