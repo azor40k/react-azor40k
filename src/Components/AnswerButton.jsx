3c1696f0e5b2d94178c1bd0d1961dea4fe754e70
ProLyfe <rayane.prolyfe@gmail.com>
2021-02-06 21:46:08 +0100

    [add] correct and wrong anwsers sounds

--- src/Components/AnswerButton.jsx
+++ src/Components/AnswerButton.jsx
@@ -1,9 +1,16 @@
 import '../Sass/components/AnswerButton.scss';
+import wrongSound from '../Sounds/wrong.mp3';
+import correctSound from '../Sounds/correct.mp3';
+import useSound from 'use-sound';
 
 const AnswerButton = ({ color, answer, setQuestionNb, changeToGray, setBaseColors, setScore, setQuestionIncrement, disableButton, setDisableButton }) => {
 
+  const [correct] = useSound(correctSound);
+  const [wrong] = useSound(wrongSound);
+
   const submitAnswer = () => {
         answer[1] === true ? setScore(prevScore => prevScore + 1) : console.log('FAUX');
+        answer[1] === true ? correct() : wrong();
         setDisableButton(true)
         changeToGray();
         setTimeout(() => {
@@ -14,7 +21,9 @@ const AnswerButton = ({ color, answer, setQuestionNb, changeToGray, setBaseColor
         }, 3000)         
   }
   return (
+        <>
         <button disabled={disableButton} className={ `AnswerButton ${color}`} onClick={submitAnswer}>{answer[0]}</button>
+      </>
 )};
 
 export default AnswerButton;
