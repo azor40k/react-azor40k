d3d71eaebf6faec6b79e4765e8287e857c013219
ProLyfe <rayane.prolyfe@gmail.com>
2021-02-06 17:28:59 +0100

    [improve] prevent click on others button when submit

--- src/Components/AnswerButton.jsx
+++ src/Components/AnswerButton.jsx
@@ -1,21 +1,21 @@
 import '../Sass/components/AnswerButton.scss';
 import { useState } from 'react';
 
-const AnswerButton = ({ color, answer, setQuestionNb, changeToGray, setBaseColors, setScore, setQuestionIncrement }) => {
+const AnswerButton = ({ color, answer, setQuestionNb, changeToGray, setBaseColors, setScore, setQuestionIncrement, disableButton, setDisableButton }) => {
 
   const submitAnswer = () => {
-      //   const targetAnswer = answer[1]
         answer[1] === true ? setScore(prevScore => prevScore + 1) : console.log('FAUX');
-        
+        setDisableButton(true)
         changeToGray();
         setTimeout(() => {
             setQuestionNb(prev => prev + 1 );
             setBaseColors(["red", "blue", "orange", "green"]);
             setQuestionIncrement(prevState => prevState + 1);  
-        }, 1000)         
+            setDisableButton(false);
+        }, 3000)         
   }
   return (
-        <button className={ `AnswerButton ${color}`} onClick={submitAnswer}>{answer[0]}</button>
+        <button disabled={disableButton} className={ `AnswerButton ${color}`} onClick={submitAnswer}>{answer[0]}</button>
 )};
 
 export default AnswerButton;
--- src/Components/Answers.jsx
+++ src/Components/Answers.jsx
@@ -6,19 +6,20 @@ const Answers = ({ setQuestionNb, answers, setQuestionIncrement, setScore }) => 
     
     const [baseColors, setBaseColors] = useState(["red", "blue", "orange", "green"]);
     const { answer1, answer2, answer3, answer4 } = answers;
+    const [disableButton, setDisableButton] = useState(false)
     
     const changeToGray = () => {
-            console.log('BIM')
+        console.log('BIM')
         setBaseColors(["grey", "grey", "grey", "grey"])
         console.log(baseColors)
     }
 
     return (
         <section className="Answers">
-            <AnswerButton color={baseColors[0]} answer={answer1} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement}/>
-            <AnswerButton color={baseColors[1]} answer={answer2} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement}/>
-            <AnswerButton color={baseColors[2]} answer={answer3} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement}/>
-            <AnswerButton color={baseColors[3]} answer={answer4} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement}/>
+            <AnswerButton color={baseColors[0]} answer={answer1} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
+            <AnswerButton color={baseColors[1]} answer={answer2} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
+            <AnswerButton color={baseColors[2]} answer={answer3} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
+            <AnswerButton color={baseColors[3]} answer={answer4} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
         </section>
 )};
 
