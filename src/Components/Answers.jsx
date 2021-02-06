cc6ba38748fd9df93ed0447687fc1fdab90d0d8d
ProLyfe <rayane.prolyfe@gmail.com>
2021-02-06 22:34:03 +0100

    [add] randomize propositions answers

--- src/Components/Answers.jsx
+++ src/Components/Answers.jsx
@@ -5,19 +5,27 @@ import AnswerButton from './AnswerButton'
 const Answers = ({ setQuestionNb, answers, setQuestionIncrement, setScore }) => {
     
     const [baseColors, setBaseColors] = useState(["red", "blue", "orange", "green"]);
-    const { answer1, answer2, answer3, answer4 } = answers;
     const [disableButton, setDisableButton] = useState(false)
     
+    const randomizer = Object.values(answers).splice(2, 6);
+
+    for(let i = randomizer.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = randomizer[i]
+        randomizer[i] = randomizer[j]
+        randomizer[j] = temp
+      };
+
     const changeToGray = () => {
         setBaseColors(["grey", "grey", "grey", "grey"])
     };
 
     return (
         <section className="Answers">
-            <AnswerButton color={baseColors[0]} answer={answer1} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
-            <AnswerButton color={baseColors[1]} answer={answer2} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
-            <AnswerButton color={baseColors[2]} answer={answer3} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
-            <AnswerButton color={baseColors[3]} answer={answer4} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
+            <AnswerButton color={baseColors[0]} answer={randomizer[0]} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
+            <AnswerButton color={baseColors[1]} answer={randomizer[1]} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
+            <AnswerButton color={baseColors[2]} answer={randomizer[2]} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
+            <AnswerButton color={baseColors[3]} answer={randomizer[3]} setQuestionNb={setQuestionNb} changeToGray={changeToGray} setBaseColors={setBaseColors} setScore={setScore} setQuestionIncrement={setQuestionIncrement} disableButton={disableButton} setDisableButton={setDisableButton}/>
         </section>
 )};
 
