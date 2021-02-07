e155593a5eb9a630b731c9363144a06fa4806e06
ProLyfe <rayane.prolyfe@gmail.com>
2021-02-07 01:55:44 +0100

    [add] page 404

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import naruto from '../Assets/naruto.jpg';
+import '../Sass/components/NotFound.scss';
 
 const NotFound = () => {
     let location = useLocation();
 
     return (
-        <section>
-            <h3>
-                No match for <code>{location.pathname}</code>
-            </h3>
+        <section className="notFound-page">
+            <h1 className="notFound-page-title">
+                Erreur 404 - Page inexistante
+            </h1>
+            <img src={naruto} alt="404 naruto" className="notFound-page-image"/>
         </section>
     )
 }
