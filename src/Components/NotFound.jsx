15f4a6bd4ec44de97c4ba7f4c6f2677cd13f90e3
azor40k <carandang.axel@gmail.com>
2021-02-07 03:15:18 +0100

    [fix] 404 issues

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import naruto from '../Assets/naruto.jpg';
 import '../Sass/components/NotFound.scss';
 
 const NotFound = () => {
-    let location = useLocation();
 
     return (
         <section className="notFound-page">
